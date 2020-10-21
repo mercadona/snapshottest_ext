60583115d7975e1cfa01eece6ac83357a0296945
Francisco Rubin <pancho_111203@hotmail.com>
2020-10-21 14:43:52 +0000

    fix integration with pandas

--- snapshottest_ext/dataframe.py
+++ snapshottest_ext/dataframe.py
@@ -1,23 +1,35 @@
-"""
-store pandas as pyarrow
-"""
+import json
+from io import BytesIO
 
 import pandas as pd
-import pyarrow as pa
 from snapshottest.formatter import Formatter
 from snapshottest.formatters import BaseFormatter
 
 
+def get_timestamp_columns(df):
+    object_cols = [
+        col for col, dtype in df.dtypes.items() if dtype == 'object'
+    ]
+    timestamp_cols = [
+        col for col in object_cols if isinstance(
+            df[col].iloc[0], pd.Timestamp)]
+    return timestamp_cols
+
+
 def pandas_to_bytes(df: pd.DataFrame) -> str:
-    context = pa.default_serialization_context()
-    df_bytestring = context.serialize(df).to_buffer().to_pybytes()
-    return df_bytestring
+    buffer = BytesIO()
+    df.to_parquet(buffer, use_deprecated_int96_timestamps=True)
+    buffer.seek(0)
+    buffer_bytes = buffer.read()
+    dtypes_str = json.dumps(df.dtypes.astype(str).to_dict())
+    return buffer_bytes + bytes(f'//SEP?//{dtypes_str}', 'utf-8')
 
 
 def bytes_to_pandas(raw_bytes) -> pd.DataFrame:
-    context = pa.default_serialization_context()
-    original_df = context.deserialize(raw_bytes)
-    return original_df
+    data_bytes, dtypes_bytes = raw_bytes.split(bytes('//SEP?//', 'utf-8'))
+    df = pd.read_parquet(BytesIO(data_bytes))
+    dtypes = json.loads(dtypes_bytes.decode())
+    return df.astype(dtypes)
 
 
 class PandasSnapshot(object):
@@ -44,18 +56,26 @@ class PandasFormatter(BaseFormatter):
         """ store pd.DataFrame as bytes in snapshot file"""
         return pandas_to_bytes(pandas_snap.value)
 
-    #def get_imports(self):
-    #    """not useful in this one, because we do not deserialize from the bytes directly"""
-    #    return ('pandasFormatter', 'PandasSnapshot'),
-
-    def assert_value_matches_snapshot(self, test, test_value: PandasSnapshot, snapshot_value, formatter):
+    def assert_value_matches_snapshot(
+            self,
+            test,
+            test_value: PandasSnapshot,
+            snapshot_value,
+            formatter):
         """
         :param test:
         :param test_value: the value in snapshot.assert_mach(value)
         :param snapshot_value: the value of format.store (after load)
         """
-        prev_df = bytes_to_pandas(snapshot_value)  # deserialize from bytes to pd.DataFrame
-        pd.testing.assert_frame_equal(test_value.value, prev_df)
+        prev_df = bytes_to_pandas(
+            snapshot_value)  # deserialize from bytes to pd.DataFrame
+        timestamp_columns = get_timestamp_columns(test_value.value)
+        nontimestamp_columns = list(
+            set(test_value.value.columns).difference(timestamp_columns))
+
+        pd.testing.assert_frame_equal(
+            test_value.value[nontimestamp_columns],
+            prev_df[nontimestamp_columns])
 
 
 Formatter.register_formatter(PandasFormatter())
